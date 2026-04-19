9e00482fa0dbaeb550ad48c50c80b9826e3470c4
Kadie Jenkins <179417967+kdelise@users.noreply.github.com>
2026-04-19 00:14:32 -0400

    Add Employee class finished

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -8,5 +8,49 @@ package model;
  * 
  */
 public class Employee {
+    private int empID, addressID;
+    private String fname, lname, email, hiredate, SSN;
+    private double salary;
+
+    public Employee(int empID, int addressID, String fname, String, lname, String email, String hiredate, String SSN, double salary) {
+        this.empID = empID;
+        this.addressID = addressID;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.hiredate = hiredate;
+        this.SSN = SSN;
+        this.salary = salary;
+    }
+
+    public int getempID() {
+        return empID;
+    }
+    public int getaddressID() {
+        return addressID;
+    }
+    public String getfname() {
+        return fname;
+    }
+    public String getlname() {
+        return lname;
+    }
+    public String getemail() {
+        return email;
+    }
+    public String gethiredate() {
+        return hiredate;
+    }
+    public String getSSN() {
+        return SSN;
+    }
+    public double getsalary() {
+        return salary;
+    }
+
+    public String toString() {
+        return empID + "," + fname + "," + lname + "," + email + "," +
+               hiredate + "," + salary + "," + SSN + "," + addressID;
+    }
     
 }
