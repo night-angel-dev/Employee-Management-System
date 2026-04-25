c75cdef2494e42c0dac794047d14e1870b11c52e
Kadie Jenkins <179417967+kdelise@users.noreply.github.com>
2026-04-25 06:34:14 -0400

    Initial ReportGenerator

--- src/controller/ReportGenerator.java
+++ src/controller/ReportGenerator.java
@@ -5,5 +5,37 @@ package controller;
  *  
  */
 public class ReportGenerator {
-    
+
+    private DataAccessLayer dal;
+    private User currentUser;
+
+    public ReportGenerator(DataAccessLayer dal, User currentUser) {
+        this.dal = dal;
+        this.currentUser = currentUser;
+    }
+
+    public void getMyPayHistory() {
+        int empID = currentUser.getEmpID();
+        dal.getPayHistory(empID);
+    }
+
+    public void getPayHistory(int empID) {
+        if (currentUser.getRole() == User.Role.HR_ADMIN) {
+            dal.getPayHistory(empID);
+        } else {
+            System.out.println("Access denied.");
+        }
+    }
+
+    public void getTotalPayByJobTitle(int month, int year) {
+        dal.getTotalPayByJobTitle(month, year);
+    }
+
+    public void getTotalPayByDivision(int month, int year) {
+        dal.getTotalPayByDivision(month, year);
+    }
+
+    public void getNewHiresByDateRange(String start, String end) {
+        dal.getNewHiresByDateRange(start, end);
+    }
 }
